91a3c09cc8f45a62e28aad5fd6d78670386e53e0
Gökhan esen <gokhan_esen@hotmail.com.tr>
2025-05-26 12:02:36 +0300

    create product commands

---
+++ ECommerceAPI.Application/Features/Products/Commands/CreateProductCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceAPI.Application.Features.Products.Commands
+{
+    public class CreateProductCommand:IRequest<CreateProductResponse>
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; }
+
+    }
+}
